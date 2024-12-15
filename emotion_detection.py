4fdef74cc9e0ea90f1e0b5dd7aa98b3c186f3b3b
marcndo <ndowahmarcel@gmail.com>
2024-12-15 03:52:05 -0500

    complete emotion detector

--- emotion_detection.py
+++ emotion_detection.py
@@ -14,5 +14,18 @@ def emotion_detector(text_to_analyze):
     doc = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, headers=headers, json=doc, timeout=10)
     
-    # convert to dictionary
-    respond_dict = json.loads(response.text)
+    # Extract required 
+    response_dict = json.loads(response.text)
+    emotions = response_dict.get("emotionPredictions", [{}])[0].get("emotion", {}) 
+    required_emotions = { "anger": emotions.get("anger", 0), 
+    "disgust": emotions.get("disgust", 0), 
+    "fear": emotions.get("fear", 0), 
+    "joy": emotions.get("joy", 0), 
+    "sadness": emotions.get("sadness", 0) } 
+
+    # Find the dominant emotion 
+    dominant_emotion = max(required_emotions, key=required_emotions.get) 
+    output = { "text": text_to_analyze, "emotions": required_emotions, "dominant_emotion": dominant_emotion } 
+    return output
+
+
