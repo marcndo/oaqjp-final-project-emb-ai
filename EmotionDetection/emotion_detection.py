62f7de39735722a07813fbda4b29fb0f826fd71e
marcndo <ndowahmarcel@gmail.com>
2024-12-16 07:57:17 -0500

    package application

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,29 @@
+"""Classifies emotion expressed in text and briefs about the author's
+current state
+"""
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """Classify emotions expressed in text.
+    Parameters:
+        text_to_analyze: str
+    """
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    doc = {'raw_document': {'text': text_to_analyze}}
+    response = requests.post(url, json = doc, headers = header, timeout=10)
+    
+    # convert response to dictionary
+    response_dict = json.loads(response.text)
+    emotions = response_dict.get("emotionPredictions", [{}])[0].get("emotion", {})
+    required_emotions = {"anger": emotions.get("anger", 0),
+                         "disgust": emotions.get("disgust", 0),
+                         "fear": emotions.get("fear", 0),
+                         "joy": emotions.get("joy", 0),
+                         "sadness": emotions.get("sadness", 0)}
+
+    # Find the dominant emotion
+    dominant_emotion = max(required_emotions, key=required_emotions.get)
+    output = {"text": text_to_analyze, "emotions": required_emotions, "dominant_emotion": dominant_emotion}
+    return output
