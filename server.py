ebe4bba4758a1427114ff1e903ae2115cae16913
marcndo <ndowahmarcel@gmail.com>
2024-12-16 23:31:58 -0500

    format code

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-"""router file for emotion detection
-"""
+"""router file for emotion detection"""
+
 from flask import Flask, request, jsonify, render_template
-from EmotionDetection.emotion_detection import emotion_detector 
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
+
 @app.route("/")
 def home():
+    """route user to the emotion detector page"""
     return render_template("index.html")
 
 @app.route("/emotionDetector", methods=["GET"])
 def emotion_detect():
+    """process data from the emotion detection page"""
     text_to_analyze = request.args.get("textToAnalyze", "")
-    logging.debug(f"Text to analyze: {text_to_analyze}")
-    
-    # Handle blank entries
     if not text_to_analyze.strip():
         result = {
             "text": text_to_analyze,
@@ -22,12 +22,9 @@ def emotion_detect():
                 "disgust": None,
                 "fear": None,
                 "joy": None,
-                "sadness": None,
-            },
-            "dominant_emotion": None
-        }
+                "sadness": None,},
+            "dominant_emotion": None }
         return jsonify(result), 400
-
     result = emotion_detector(text_to_analyze)
     try:
         if result and result["dominant_emotion"] is not None:
@@ -35,18 +32,14 @@ def emotion_detect():
             response_message = (
                 f"For the given statement, the system response is "
                 f"'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, "
-                f"'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}. "
+                f"'fear': {emotions['fear']}, 'joy': {emotions['joy']} "
+                f"and 'sadness': {emotions['sadness']}."
                 f"The dominant emotion is {result['dominant_emotion']}."
             )
             return jsonify({"message": response_message})
-        else:
-            return jsonify({"error": "Invalid text! Please try again."}), 400
+        return jsonify({"error": "Invalid text! Please try again."}), 400
     except KeyError as e:
-        logging.error(f"Missing key in result: {e}")
         return jsonify({"error": f"Missing key in result: {e}"}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
-
